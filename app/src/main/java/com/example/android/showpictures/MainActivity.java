df9653af031ddb116f26923b70aba0c8682bf551
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-10-19 19:38:35 -0700

    add a list

--- app/src/main/java/com/example/android/showpictures/MainActivity.java
+++ app/src/main/java/com/example/android/showpictures/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.android.showpictures;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPhoto = (TextView) findViewById(R.id.tv_photo_data);
+
+        String[] dummyPhotoData = {
+                "abc.jpg",
+                "def.jpg",
+        };
+
+        for (String dummyPhoto : dummyPhotoData) {
+            mPhoto.append(dummyPhoto + "\n\n\n");
+        }
     }
 }
